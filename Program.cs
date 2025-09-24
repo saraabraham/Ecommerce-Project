bb01005e7ad6a6ed6b1c417edd92263b12cfecd6
Sara Abraham <sara.abraham12@gmail.com>
2025-09-24 09:53:35 +0200

    Added Views

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿using ElectronicsStoreMVC.Models;
-using ElectronicsStoreMVC.Services;
-using Microsoft.AspNetCore.Identity;
+﻿using ElectronicsStoreMVC.Services;
 using Microsoft.EntityFrameworkCore;
-using MySql.EntityFrameworkCore.Extensions; // <-- Add this at the top
+using ElectronicsStoreMVC.Models;
+using Microsoft.AspNetCore.Identity;
+using sib_api_v3_sdk.Client;
+using DotNetEnv;
 
 
 var builder = WebApplication.CreateBuilder(args);
+Env.Load();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySQL(connectionString); // <-- correct method for Oracle provider
+    options.UseMySQL(connectionString);
 });
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(
     options =>
@@ -23,6 +25,20 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(
         options.Password.RequireLowercase = false;
     }
     ).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+// Bind secrets from configuration
+builder.Services.Configure<BrevoSettings>(builder.Configuration.GetSection("BrevoSettings"));
+builder.Services.Configure<PayPalSettings>(builder.Configuration.GetSection("PayPalSettings"));
+
+// Apply Brevo API key to SDK
+var brevoApiKey = builder.Configuration["BrevoSettings:ApiKey"];
+if (string.IsNullOrEmpty(brevoApiKey))
+{
+    throw new InvalidOperationException("Brevo API key is missing! Check your .env file or environment variables.");
+}
+Configuration.Default.ApiKey.Add("api-key", brevoApiKey);
+
+//Configuration.Default.ApiKey.Add("api-key", builder.Configuration["BrevoSettings:ApiKey"]);
 
 var app = builder.Build();
 
@@ -38,6 +54,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
@@ -45,5 +62,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
 
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetService(typeof(UserManager<ApplicationUser>)) as UserManager<ApplicationUser>;
+    var roleManager = scope.ServiceProvider.GetService(typeof(RoleManager<IdentityRole>)) as RoleManager<IdentityRole>;
+    await DatabaseInitializer.SeedDataAsync(userManager, roleManager);
+
+
+}
+
+app.Run();
